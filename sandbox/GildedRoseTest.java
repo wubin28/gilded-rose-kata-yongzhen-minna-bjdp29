92e11a7cf6ffbc3dbaadba93916db21b9bc0a3bb
wolf_85BD27F332 <wolf@cyber-dojo.org>
2015-11-07 06:38:59 +0000

    12

--- sandbox/GildedRoseTest.java
+++ sandbox/GildedRoseTest.java
@@ -30,7 +30,7 @@ public class GildedRoseTest {
 
     @Test
     public void testSulfuras() {
-        Item[] items = new Item[] { new Item("Sulfuras", 5, 80) };
+        Item[] items = new Item[] { new Item("Sulturas", 5, 80) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(80, app.items[0].quality);
