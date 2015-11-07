51eba5e6f548661b6d9b525d96244b33ed68aebf
wolf_85BD27F332 <wolf@cyber-dojo.org>
2015-11-07 07:00:34 +0000

    34

--- sandbox/GildedRoseTest.java
+++ sandbox/GildedRoseTest.java
@@ -37,7 +37,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void testQualityIncreaseForAgedBrie() {
+    public void testQualityIncreaseForAgedBrieAlwaysLess50() {
         Item[] items = new Item[] { new Item("Aged Brie", 5, 50) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
